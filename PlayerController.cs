48a2a66ae48bd1729fac68aae2d10235d7854826
Shavonne Carson <59718290+gizzieiam@users.noreply.github.com>
2021-01-01 19:47:08 -0800

    player is mobile

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+
+
+    Vector3 velocity;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector3 playerMovement = new Vector3(moveX, 0f, moveY) * speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
     }
 }
